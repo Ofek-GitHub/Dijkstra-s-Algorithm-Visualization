19bd7c0fbcffb74bed385fbf5c55dc86665c0077
Ofek-GitHub <ofeksimantov53@gmail.com>
2022-12-06 20:55:54 +0200

    Installed pygame,Made a black window

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 from tkinter import messagebox, Tk
 import pygame
+import sys
+
+window_width = 500
+window_height = 500
+
+window = pygame.display.set_mode((window_width, window_height))
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            # quit window 
+            if event.type == pygame.QUIT:   # we Creating black window 
+                pygame.quit()
+                sys.exit()
+        
+        window.fill((0, 0, 0))
+        pygame.display.flip()
+main()
