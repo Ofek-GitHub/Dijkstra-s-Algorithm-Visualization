c4bf1019210b8e90e80b64fe40328b5dbd26ae49
Ofek-GitHub <ofeksimantov53@gmail.com>
2022-12-14 14:44:00 +0200

    made a cleaner code, i will delete the main01..

--- main.py
+++ main.py
@@ -1,91 +1,102 @@
 from tkinter import messagebox, Tk
 import pygame
+import math
 import sys
 
-window_width = 500              # the width of the window
-window_height = 500             # the height of the window 
-
+window_width = 500
+window_height = 500
 window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Dijkstra's PATH FINDING ALGORITHM !")
 
 columns = 25
-rows = 25
-box_width = window_width // columns     # size of the boxes
-box_height = window_height // rows      # size of the boxes
-grid = []
+rows = 25               
+
+box_width = window_width //columns # this will be the width of each tiny box inside the window
+box_height = window_height //rows # this will be the height of each, #essentially we took the large grid and divide it with the columns and rows to tiny boxes
+
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0 ,0, 255)
+YELLOW = (255,255,0)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+PURPLE = (128,0.128)
+ORANGE = (255,165,0)
+GREY = (128,128,128)
+TURQUOISE = (64,224,208)            # pulled all the color values 
+
+grid = []           # this empty array is going to store all this individual boxes in the grid
 
 class Box:
-    def __init__(self, i, j):
+    def __init__(self,i, j): # we identify the box itself from the grid with I, and j.  i will through the rows, j will go through the columns.
         self.i = i
         self.j = j
-        self.start = False  # where we start our search
-        self.wall = False   # wall to close paths to the target
-        self.target = False # where we want to go from out start to target
-    def draw(self, window, color):
-        pygame.draw.rect(window, color,(self.i * box_width, self.j * box_height, box_width - 2, box_height -2)) #some reason i must subtract to see the grid boxes 
-         
+        self.start = False
+        self.wall = False
+        self.target = False
+    def draw(self, window, color):  # here we mark the box as being drawn.  #in window we talk about the grid itself
+        pygame.draw.rect(window, color, (self.i * box_width, self.j * box_height, box_width - 2, box_height -2)) #some sort of margin around the grid border
         
-# create the grid 
-for i in range(columns):
-    arr = [] 
+        
+# create the grid
+for i in range(columns):            # short explanation, we save all the instance variables of the box within the grid array.
+    arr = []
     for j in range(rows):
         arr.append(Box(i, j))
-    grid.append(arr)
-
-start_box = grid[0][0]
-start_box.start = True
+    grid.append(arr)                # in the main we will draw the boxes themselves.
+    
 
 def main():
     begin_search = False
     target_box_set = False
-    start_box_set = False
+    target_box_start = False
     while True:
         for event in pygame.event.get():
-            # quit window 
-            if event.type == pygame.QUIT:   # we Creating black window 
-                pygame.quit()               # quit the window thats what this block does.
+            #quit window
+            if event.type == pygame.QUIT:
+                pygame.quit()
                 sys.exit()
-            # mouse controls
-            elif event.type == pygame.MOUSEMOTION: 
+            elif event.type == pygame.MOUSEMOTION:
                 x = pygame.mouse.get_pos()[0]
                 y = pygame.mouse.get_pos()[1]
-                # Draw Wall
-                if event.buttons[0]:    # left mouse button is pressed.
-                    i = x // box_width  # we mark the boxes width that become walls.
-                    j = y // box_height # we mark the boxes height that become walls.
-                    grid[i][j].wall = True
-                # Set Target Position
-                if event.buttons[2] and not target_box_set: # we checking we havent alrdy set a target
-                    i = x // box_width
+                # draw wall
+                if event.buttons[0]:
+                    i = x // box_width 
                     j = y // box_height
-                    target_box = grid[i][j] # where in the grid the target is located
+                    grid[i][j].wall = True
+                # set target position
+                if event.buttons[2] and not target_box_set:
+                    i = x //box_width
+                    j = y //box_height
+                    target_box = grid[i][j]
                     target_box.target = True
                     target_box_set = True
-                # Set Start Position
-                if event.buttons[2] and not start_box_set: # we checking we havent alrdy set a target
-                    i = x // box_width
-                    j = y // box_height
-                    start_box = grid[i][j] # where in the grid the start is located
-                    start_box.target = True
-                    start_box_set = True
-                    
-            # start the algorithm
-            if event.type == pygame.KEYDOWN and target_box_set:
+                # set start position
+                if event.buttons[1] and not target_box_start:
+                    i = x //box_width
+                    j = y //box_height
+                    start_box = grid[i][j]
+                    start_box.start = True
+                    target_box_start = True
+            # start algorithm
+            if event.type == pygame.KEYDOWN and target_box_set and target_box_start:
                 begin_search = True
-                
+                        
                     
-            
-        
-        window.fill((0, 0, 0))
-        
+        window.fill(BLACK)
+    
         for i in range(columns):
             for j in range(rows):
-                box = grid[i][j]
-                box.draw(window, (60, 60, 60))
+                box = grid[i][j]        # there is a BOX in the grid we give it to the object box
+                box.draw(window, GREY)  # the grid is being drawn with boxes
                 if box.start:
-                    box.draw(window, (0,100,100))
-                if box.wall:
-                    box.draw(window, (0,200,200))
+                    box.draw(window,BLUE)
                 if box.target:
-                    box.draw(window, (200, 200,200))
-        pygame.display.flip()               # can use display.update() as well.
+                    box.draw(window,GREEN)
+                if box.wall:
+                    box.draw(window,ORANGE)
+        
+        pygame.display.update()
+        
 main()
+    
--- main01.py
+++ main01.py
@@ -1,16 +1,22 @@
+from tkinter import messagebox, Tk
 import pygame
 import math
 import sys
-from queue import PriorityQueue
 
-WiDTH = 800
-window = pygame.display.set_mode((WiDTH, WiDTH))
+window_width = 500
+window_height = 500
+window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Dijkstra's PATH FINDING ALGORITHM !")
 
+columns = 25
+rows = 25               
+
+box_width = window_width //columns # this will be the width of each tiny box inside the window
+box_height = window_height //rows # this will be the height of each, #essentially we took the large grid and divide it with the columns and rows to tiny boxes
 
 RED = (255,0,0)
 GREEN = (0,255,0)
-BLUE = (0,255,0)
+BLUE = (0 ,0, 255)
 YELLOW = (255,255,0)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
@@ -19,34 +25,78 @@ ORANGE = (255,165,0)
 GREY = (128,128,128)
 TURQUOISE = (64,224,208)            # pulled all the color values 
 
-class Node: # we need to know the color to identify if we are start position or end position or wall.
-    def __init__(self, row, col, width, total_rows):
-        self.row = row
-        self.col = col
-        self.x = row * width        # WIDTH = 800 we will have aporx 50 cubes so width = 800/cubes and thats the width of the cube.
-        self.y = col * width        # self.x means the row of the starting cube and self.y means the col of the starting cube
-        self.color = GREY 
-        self.width = width
-        self.total_rows = total_rows
-        self.neighbors = []
+grid = []           # this empty array is going to store all this individual boxes in the grid
+
+class Box:
+    def __init__(self,i, j): # we identify the box itself from the grid with I, and j.  i will through the rows, j will go through the columns.
+        self.i = i
+        self.j = j
+        self.start = False
+        self.wall = False
+        self.target = False
+    def draw(self, window, color):  # here we mark the box as being drawn.  #in window we talk about the grid itself
+        pygame.draw.rect(window, color, (self.i * box_width, self.j * box_height, box_width - 2, box_height -2)) #some sort of margin around the grid border
         
-    def get_positions(self):
-        return self.row, self.col   #We will index the positions using row and col
+        
+# create the grid
+for i in range(columns):            # short explanation, we save all the instance variables of the box within the grid array.
+    arr = []
+    for j in range(rows):
+        arr.append(Box(i, j))
+    grid.append(arr)                # in the main we will draw the boxes themselves.
     
-    def is_start(self): 
-        return self.colored == BLUE
-    
-    def is_target(self):
-        return self.colored == GREEN
-    
-    def is_barrier(self):
-        return self.colored == BLACK
-    def reset(self):
-        self.colored = GREY
-    
+
+def main():
+    begin_search = False
+    target_box_set = False
+    target_box_start = False
     while True:
         for event in pygame.event.get():
-            # quit window 
-            if event.type == pygame.QUIT:   # we Creating black window 
-                pygame.quit()               # quit the window thats what this block does.
+            #quit window
+            if event.type == pygame.QUIT:
+                pygame.quit()
                 sys.exit()
+            elif event.type == pygame.MOUSEMOTION:
+                x = pygame.mouse.get_pos()[0]
+                y = pygame.mouse.get_pos()[1]
+                # draw wall
+                if event.buttons[0]:
+                    i = x // box_width 
+                    j = y // box_height
+                    grid[i][j].wall = True
+                # set target position
+                if event.buttons[2] and not target_box_set:
+                    i = x //box_width
+                    j = y //box_height
+                    target_box = grid[i][j]
+                    target_box.target = True
+                    target_box_set = True
+                # set start position
+                if event.buttons[1] and not target_box_start:
+                    i = x //box_width
+                    j = y //box_height
+                    start_box = grid[i][j]
+                    start_box.start = True
+                    target_box_start = True
+            # start algorithm
+            if event.type == pygame.KEYDOWN and target_box_set and target_box_start:
+                begin_search = True
+                        
+                    
+        window.fill(BLACK)
+    
+        for i in range(columns):
+            for j in range(rows):
+                box = grid[i][j]        # there is a BOX in the grid we give it to the object box
+                box.draw(window, GREY)  # the grid is being drawn with boxes
+                if box.start:
+                    box.draw(window,BLUE)
+                if box.target:
+                    box.draw(window,GREEN)
+                if box.wall:
+                    box.draw(window,ORANGE)
+        
+        pygame.display.update()
+        
+main()
+    
